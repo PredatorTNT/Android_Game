96cda06b0b3a050c31125865f1dcce65905ec5c6
eberperetto <eberperetto@gmail.com>
2015-12-09 02:50:21 -0200

    Feitos Abertura, Menu e Sobre

--- GameDisco/app/src/main/java/br/com/pxgamers/gameon/discoblocks/CenaAbertura.java
+++ GameDisco/app/src/main/java/br/com/pxgamers/gameon/discoblocks/CenaAbertura.java
@@ -12,6 +12,11 @@ public class CenaAbertura extends AGScene {
     private AGSprite Logo = null;
     private int estadoLogo = 0;
 
+    private AGSprite Disco = null;
+    private int estadoDisco = 0;
+
+    private int contador = 0;
+
     //contruto de cena
     public CenaAbertura(AGGameManager vrGerente) {
         super(vrGerente);
@@ -20,13 +25,21 @@ public class CenaAbertura extends AGScene {
     //Metodo chamado sempre que a sena é exibida
     public void init() {
         setSceneBackgroundColor(1, 1, 1);
-        Logo = createSprite(R.drawable.disco_vecteur, 1, 1);
+
+        Logo = createSprite(R.drawable.logo_uri, 1, 1);
         Logo.setScreenPercent(100, 35);
         Logo.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, AGScreenManager.iScreenHeight / 2);
-        Logo.fadeIn(4000);
+        Logo.fadeIn(3000);
         estadoLogo = 0;
-        AGSoundManager.vrMusic.loadMusic("musica.mp3", true);
-        AGSoundManager.vrMusic.play();
+
+        Disco = createSprite(R.drawable.disco1, 4, 1);
+        Disco.setScreenPercent(90, 70);
+        Disco.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, AGScreenManager.iScreenHeight / 2);
+        Disco.bVisible = false;
+        Disco.addAnimation(3,true,0,3);
+        estadoDisco = 0;
+
+        AGSoundManager.vrMusic.loadMusic("musicaMenu.mp3", true);
     }
 
     //chamado após uma interrupção de um aplicativo
@@ -42,15 +55,31 @@ public class CenaAbertura extends AGScene {
     //Metodo chamado quadro a quadro(logica do jogo)
     public void loop() {
 
+        contador ++;
+
         if (estadoLogo == 0) {
             if (Logo.fadeEnded()) {
-                Logo.fadeOut(4000);
+                Logo.fadeOut(3000);
                 estadoLogo = 1;
             }
         } else {
             if (Logo.fadeEnded()) {
-                vrGameManager.setCurrentScene(1);
+
+                setSceneBackgroundColor(0.541176f, 0.168627f, 0.886275f);
+                Disco.bVisible = true;
+                AGSoundManager.vrMusic.play();
+                estadoDisco = 1;
+
+                estadoLogo = 2;
+                Logo.bVisible = false;
             }
         }
+
+        if (estadoDisco == 1) {
+            Disco.setCurrentAnimation(0);
+            estadoDisco = 2;
+        }
+
+        if (estadoDisco == 2 && estadoLogo == 2 && contador >= 500) vrGameManager.setCurrentScene(1);
     }
 }
